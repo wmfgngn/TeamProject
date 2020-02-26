44b90e4483ffb417f57353386d6c650831941e81
Isoop <61407018+Isoop@users.noreply.github.com>
2020-02-26 17:13:13 +0900

    원 게시글 삭제 Action

---
+++ src/project/action/ViewDelAction.java
@@ -0,0 +1,23 @@
+package project.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mybatis.dao.BbsDAO;
+
+public class ViewDelAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+
+
+		String b_idx = request.getParameter("b_idx");
+
+		boolean chk = BbsDAO.delBbs(b_idx);
+		request.setAttribute("value", chk);
+		
+		return "/del_bbs.jsp";
+		
+	}
+
+}
