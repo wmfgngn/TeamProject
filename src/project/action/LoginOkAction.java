db8a5d366147c407bb1a3a310d050923a3988c9c
wmfgngn <61406869+wmfgngn@users.noreply.github.com>
2020-02-26 14:19:58 +0900

    Update LoginOkAction.java

--- src/project/action/LoginOkAction.java
+++ src/project/action/LoginOkAction.java
@@ -1,7 +1,13 @@
 package project.action;
 
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.MultipartRequest;
 
 import mybatis.dao.BbsDAO;
 import project.vo.BbsVO;
@@ -13,20 +19,43 @@ public class LoginOkAction implements Action {
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		String s_id = request.getParameter("s_id");
 		String s_pw = request.getParameter("s_pw");
-		boolean res = false;
-		//System.out.println(s_id+"/"+s_pw);
-		RegVO vo = BbsDAO.login(s_id, s_pw);
-		if(vo != null) {
-			res = true;
-			request.setAttribute("res", res);
-			request.setAttribute("loVo", vo);
+		String c_type = request.getContentType();//요청한 곳으로 부터
+		
+		// MIME타입을 얻는다.
+		// - Get방식 : null
+		// - Post방식 : application/....
+		// - Post방식에 encType="multipart/form-data": multipart/....
+		//System.out.println(c_type);
+		String viewPath = "/jsp/login.jsp";
+		
+		if(c_type != null && c_type.startsWith("application/")) {
+		try {
+			boolean res = false;
+			//System.out.println(s_id+"/"+s_pw);
+			RegVO vo = BbsDAO.login(s_id, s_pw);
+			if(vo != null) {
+				res = true;
+				request.setAttribute("res", res);
+				HttpSession session = request.getSession();
+				session.setAttribute("loVo", vo);
+				
+			}else {
+				request.setAttribute("res", res);
+				
+			}
 			
-		}else {
-			request.setAttribute("res", res);
-			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return "/jsp/login_ajax.jsp";
+		//JSP경로를 반환하면 forward되도록 되어 있어서
+		// f5 즉, 화면 갱신을 하면 저장된 정보가 계속 저장된다.
+		//viewPath = "control?type=list";
+			viewPath = "/jsp/login_ajax.jsp";
+		}
+		
+		return viewPath;
 	}
 
 }
