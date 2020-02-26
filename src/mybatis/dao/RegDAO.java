771e9e76130af6366a1902ba894b59afa48444a0
o2o25252 <61407010+o2o25252@users.noreply.github.com>
2020-02-26 16:45:16 +0900

    회원정보 수정 탈퇴 DAO
    회원정보 수정 탈퇴 DAO 확인안해봄

--- src/mybatis/dao/RegDAO.java
+++ src/mybatis/dao/RegDAO.java
@@ -55,6 +55,38 @@ public class RegDAO {
 		
 		return vo;
 	}
-
+	//회원 정보 수정 
+	public static boolean crystal(String s_pw,String s_phone) {
+		boolean chk = false;
+		SqlSession ss =FactoryService.getFactory().openSession();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("s_pw",s_pw);
+		map.put("s_phone",s_phone);
+		 int cnt =ss.update("reg.crystal",map);
+		 
+		 if(cnt >0) {
+			 chk = true;
+			 ss.commit();
+		 }else
+			 ss.rollback();
+			 ss.close();
+			 
+		return chk; 
+		 
+	}
+	//회정 탈퇴
+	public static boolean taltoe(String s_pw) {
+		boolean chk = false;
+		SqlSession ss =FactoryService.getFactory().openSession();
+		int cnt = ss.update("reg.taltoe",s_pw);
+		
+		if(cnt >0) {
+			chk =true;
+			ss.commit();
+		}else
+			ss.rollback();
+			ss.close();
+		return chk;	
+	}
 	
 }
